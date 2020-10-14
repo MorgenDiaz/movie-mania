35200e4c277175eddb66c5e6dd76dcc7007a4957
Morgen Diaz <morgen@webslinger.works>
2020-10-14 13:54:19 -1000

    Created a viewmodel factory and added logic for HomeViewModel construction. Binded HomeViewModel to HomeFragment. Home screen buttons bind to navigation operations of HomeViewModel. Home screen can navigate to Moview, TvShows, and Characters screens

--- app/src/main/java/com/webslinger/portfolio/moviemania/application/home/HomeViewModel.kt
+++ app/src/main/java/com/webslinger/portfolio/moviemania/application/home/HomeViewModel.kt
@@ -1,7 +1,20 @@
 package com.webslinger.portfolio.moviemania.application.home
 
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.webslinger.portfolio.moviemania.R
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(private val navController: NavController) : ViewModel() {
+    fun navigateToMoviesScreen(){
+        navController.navigate(R.id.action_homeFragment_to_moviesFragment)
+    }
+
+    fun navigateToTvShowsScreen(){
+        navController.navigate(R.id.action_homeFragment_to_tvShowsFragment)
+
+    }
+
+    fun navigateToCharactersScreen(){
+        navController.navigate(R.id.action_homeFragment_to_charactersFragment)
+    }
 }
--- app/src/main/java/com/webslinger/portfolio/moviemania/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/webslinger/portfolio/moviemania/presentation/home/HomeFragment.kt
@@ -6,22 +6,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.webslinger.portfolio.moviemania.R
+import com.webslinger.portfolio.moviemania.application.common.ViewModelFactory
 import com.webslinger.portfolio.moviemania.application.home.HomeViewModel
+import com.webslinger.portfolio.moviemania.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
-
     companion object {
         fun newInstance() = HomeFragment()
     }
 
+    private lateinit var viewModelFactory: ViewModelFactory
     private lateinit var viewModel: HomeViewModel
+    private lateinit var dataBinding: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModelFactory = ViewModelFactory(findNavController())
+        viewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
+        dataBinding.viewModel = viewModel
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
